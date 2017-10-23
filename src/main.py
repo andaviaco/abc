4979db530a1b13e66c9eca253ba3ec8f750c6c09
Andrés Ávila <andaviaco@gmail.com>
2017-10-23 08:04:17 -0500

    print solutions for Sphere, Rastrigin and Ackley functions

--- src/main.py
+++ src/main.py
@@ -1,6 +1,29 @@
+import lib
+from swarm import ABC
+
 
 def main():
-    pass
+    abc_sphere = ABC(30, 49, lib.sphere, fn_lb=[-5, -5], fn_ub=[5, 5])
+    abc_ackley = ABC(30, 49, lib.ackley, fn_lb=[-20, -20], fn_ub=[20, 20])
+    abc_rastrigin = ABC(30, 49, lib.rastrigin, fn_lb=[-5, -5], fn_ub=[5, 5])
+
+    min_x, min_y = abc_sphere.optimize()
+    eval_result = lib.sphere([min_x, min_y])
+
+    print(f'Sphere MIN: x={min_x}, y={min_y}')
+    print(f'Sphere({min_x}, {min_y}) = {round(eval_result, 4)}')
+
+    min_x, min_y = abc_rastrigin.optimize()
+    eval_result = lib.rastrigin([min_x, min_y])
+
+    print(f'Rastrigin MIN: x={min_x}, y={min_y}')
+    print(f'Rastrigin({min_x}, {min_y}) = {round(eval_result, 4)}')
+
+    min_x, min_y = abc_ackley.optimize()
+    eval_result = lib.ackley([min_x, min_y])
+
+    print(f'Ackley MIN: x={min_x}, y={min_y}')
+    print(f'Ackley({min_x}, {min_y}) = {round(eval_result, 4)}')
 
 if __name__ == '__main__':
     main()
